9beb23cca13d89987bc4ed2dcd946048d4e46f1d
Mahesh-MSFT <maksh@microsoft.com>
2017-05-02 17:16:22 +0530

    Add project files.

---
+++ KeyVaultDemo/EnvironmentSecretManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault.Models;
+
+namespace KeyVaultDemo
+{
+    public class EnvironmentSecretManager : IKeyVaultSecretManager
+    {
+        public string GetKey(SecretBundle secret)
+        {
+            return secret.SecretIdentifier.Name;
+        }
+
+        public bool Load(SecretItem secret)
+        {
+            return secret.Identifier.Name.StartsWith("sqlconn");
+        }
+    }
+}
